4cbe12a352516c538ef19cc9c0e9882da327f3ff
Samuel Montoya <s.montoya112@uniandes.edu.co>
2025-05-12 21:37:41 -0500

    scanner completado

---
+++ p3/src/ProblemaP3.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ProblemaP3 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int numCasos = scanner.nextInt();
+        
+        for (int caso = 0; caso < numCasos; caso++) {
+            int n = scanner.nextInt();
+            int k = scanner.nextInt();
+            scanner.nextLine();
+
+            String[] cadenas = new String[n];      
+            for (int j = 0; j < n; j++) {
+                String cadena = scanner.nextLine();
+                if(cadena.length()==k){
+                    cadenas[j] = cadena;
+                }
+                else{
+                    System.err.println("Las palabras no son todas del mismo tamanio");
+                }
+            }
+        }
+        scanner.close();
+    }
+}
