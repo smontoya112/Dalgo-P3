dd2f125bafc737b48af5c10cbf2048a2d834daa6
Samuel Montoya <s.montoya112@uniandes.edu.co>
2025-05-18 22:03:42 -0500

    a

--- p3/src/ProblemaP3.java
+++ p3/src/ProblemaP3.java
@@ -1,10 +1,32 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class ProblemaP3 {
-    public String scs(String[] cadenas){
+    public static int overlap(String cadena1, String cadena2) {
+        int distancia = 0;
+        LinkedList<String> cadena1Sub = new LinkedList<>();
+        LinkedList<String> cadena2Sub = new LinkedList<>();
         
-        return null;
+        if(cadena1.contains(cadena2)) {
+            return cadena2.length();
+        }
+        else if(cadena2.contains(cadena1)) {
+            return cadena1.length();
+        }
+        else {
+            for (int a = 0; a < Math.min(cadena1.length(), cadena2.length()); a++) {
+                cadena1Sub.addFirst(String.valueOf(cadena1.charAt(cadena1.length() - 1 - a)));
+                cadena2Sub.add(String.valueOf(cadena2.charAt(a)));
+                
+                if (cadena1Sub.equals(cadena2Sub)) {
+                    distancia = a + 1; 
+                }
+            }
+        }
+        return distancia;    
     }
+
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         int numCasos = scanner.nextInt();
@@ -23,6 +45,24 @@ public class ProblemaP3 {
                 else{
                     System.err.println("Las palabras no son todas del mismo tamanio");
                 }
+            }
+            int[][] grafo = new int[n][n];
+            for (int j = 0; j < n; j++) {
+                for (int l = 0; l < n; l++) {
+                    if (j != l) {
+                        grafo[j][l] = overlap(cadenas[j], cadenas[l]);
+                    }
+                }
+            }
+
+            // Imprimir el grafo de forma bonita
+            System.out.println("Grafo de solapamientos:");
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    System.out.print(grafo[i][j]);
+                    if (j < n - 1) System.out.print(" ");
+                }
+                System.out.println();
             }
         }
         scanner.close();
